e1c270d9eb01fbe6dfe95acbfa3bacda5f1dca72
Scooter480 <noahsemailaccount@gmail.com>
2021-03-11 11:36:05 -0500

    switched to posix compliant sheel and sped up script

--- weatherico.sh
+++ weatherico.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 result=$(curl -Ss 'https://wttr.in?0&T&Q' | cut -c 16- | head -2 | xargs echo)
-cloud=$(echo "$result" | grep -E "cloud|fog" | wc -l)
-sun=$(echo "$result" | grep "clear" | wc -l)
-snow=$(echo "$result" | grep -E "snow|sleet" | wc -l)
-rain=$(echo "$result" | grep rain | wc -l)
-storm=$(echo "$result" | grep storm | wc -l)
-wind=$(echo "$result" | grep wind | wc -l)
-dead=$(echo "$result" | grep "°" | wc -l)
-[[ "$dead" = 0 ]] && exit 1
-[[ "$cloud" = 1 ]] && icon=" "
-[[ "$sun" = 1 ]] && icon=" "
-[[ "$snow" = 1 ]] && icon=" "
-[[ "$rain" = 1 ]] && icon=" "
-[[ "$storm" = 1 ]] && icon=" "
-[[ "$wind" = 1 ]] && icon=" "
+cloud=$(echo "$result" | grep -i -c -E "cloud|fog|overcast" )
+sun=$(echo "$result" | grep -i -c "clear" )
+snow=$(echo "$result" | grep -i -c -E "snow|sleet" )
+rain=$(echo "$result" | grep -i -c rain )
+storm=$(echo "$result" | grep -i -c storm )
+wind=$(echo "$result" | grep -i -c wind )
+dead=$(echo "$result" | grep -c "°" )
+[ "$dead" = 0 ] && exit 1
+[ "$cloud" = 1 ] && icon=" "
+[ "$sun" = 1 ] && icon=" "
+[ "$snow" = 1 ] && icon=" "
+[ "$rain" = 1 ] && icon=" "
+[ "$storm" = 1 ] && icon=" "
+[ "$wind" = 1 ] && icon=" "
 echo "$icon $result"
