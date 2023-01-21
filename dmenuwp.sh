215c7598ec5db06d1c00db58c6bb4391468631c8
River Dyke <riverdykemail@gmail.com>
2023-01-20 21:39:41 -0500

    cleaned up and updated

--- dmenuwp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-wallpaper=$(ls /home/$USER/pix/wallpaper | grep -E ".png|.jpg")
-chosen=$(echo "$wallpaper" | dmenu -i -p "Which wallpaper?")
-if [ "$chosen" = "" ]; then exit 1; fi
-fit=$(echo "zoom\ntile\nstretch\ncenter\nmaximize" | dmenu -i -p "Fit type: ")
-if [ "$fit" = "" ]; then exit 1; fi
-echo "xwallpaper --$fit /home/$USER/pix/wallpaper/$chosen" > /home/$USER/pix/wallpaper/wallpaper.sh
-xwallpaper --$fit "/home/$USER/pix/wallpaper/$chosen" && exit 0
